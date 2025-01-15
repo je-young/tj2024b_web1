929b030d5d1f4bfb45a9f6258976136b63299e01
je-young <jey2965@gmail.com>
2025-01-15 18:25:25 +0900

    test

---
+++ tj2024b_web1/src/main/java/day01/Example3.java
@@ -0,0 +1,18 @@
+package day01;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Example3 extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("방법3 : 안녕 나는 서블릿이야3");
+	}
+
+}
---
+++ tj2024b_web1/src/main/java/day01/Task3.java
@@ -0,0 +1,18 @@
+package day01;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Task3 extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("방법3 : URL를 매핑하는 Task3 클래스를 만드시오. (클래스생성/web.xml설정)");
+	}
+
+}
